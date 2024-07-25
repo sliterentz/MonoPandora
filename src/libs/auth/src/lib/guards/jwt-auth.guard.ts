eb22b0fdd46cdc225e047e4cd87e7c36042398f9
Arief Hakim <sliterentz@gmail.com>
2024-07-25 21:37:11 +0700

    feat(Add new helper and update auth guard): Add new hash helper, pagination helper, http exception helper, and add new auth guard

--- src/libs/auth/src/lib/guards/jwt-auth.guard.ts
+++ src/libs/auth/src/lib/guards/jwt-auth.guard.ts
@@ -1,48 +1,97 @@
-import { ExecutionContext, Injectable, UnauthorizedException, Request } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtService } from '@nestjs/jwt';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { UserService } from '../user/user.service';
+// import { JwtService } from '@nestjs/jwt';
+import { ExtractJwt } from 'passport-jwt';
+// import { FastifyRequest, FastifyReply } from 'fastify';
+// import { UserService } from '../user/user.service';
+import { SKIP_AUTH } from '../types/constants';
+import { InvalidTokenException } from '../helpers/http-exception.helper';
+import { TokenService } from '../token.service';
+import { TokenType } from '../types/tokens';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(
     private reflector: Reflector,
-    private jwtService: JwtService,
+    private tokenService: TokenService,
+    // private jwtService: JwtService,
     ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: process.env['JWT_SECRET']
-    });
+      super()
+    // super({
+    //   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    //   ignoreExpiration: false,
+    //   secretOrKey: process.env['JWT_SECRET']
+    // });
   }
 
-  override canActivate(context: ExecutionContext) {
-     const request = context.switchToHttp().getRequest();
-     const token = this.extractTokenFromHeader(request);
+  // override canActivate(context: ExecutionContext) {
+  //    const request = context.switchToHttp().getRequest();
+  //    const token = this.extractTokenFromHeader(request);
 
+  //   const isPublic = this.reflector.getAllAndOverride('isPublic', [
+  //     context.getHandler(),
+  //     context.getClass(),
+  //   ]);
+  // //
+  //   if (isPublic) return true;
+  // //
+  //   return super.canActivate(context);
+  // }
+
+  // private extractTokenFromHeader(request: FastifyRequest): string | undefined {
+
+  //   let accessToken = '';
+  //   const tokenSegmen = request.headers.authorization?.split(" ");
+  //   if(tokenSegmen) {
+  //     accessToken = tokenSegmen[1];
+  //   }
+
+  //   return accessToken;
+  // }
+
+  /**
+   * Verify the token is valid
+   * @param context {ExecutionContext}
+   * @returns super.canActivate(context)
+   */
+  override canActivate(context: ExecutionContext) {
+    const skipAuth = this.reflector.getAllAndOverride<boolean>(SKIP_AUTH, [context.getHandler(), context.getClass()]);
     const isPublic = this.reflector.getAllAndOverride('isPublic', [
       context.getHandler(),
       context.getClass(),
     ]);
-  //
+
     if (isPublic) return true;
-  //
+
+    if (skipAuth) {
+      return true;
+    }
+
+    const accessToken = ExtractJwt.fromAuthHeaderAsBearerToken()(context.switchToHttp().getRequest());
+    if (!accessToken) {
+      throw new InvalidTokenException();
+    }
+
+    const payload = this.tokenService.verifyToken(accessToken, TokenType.AccessToken);
+    if (!payload) {
+      throw new UnauthorizedException();
+    }
     return super.canActivate(context);
   }
 
-  private extractTokenFromHeader(request: FastifyRequest): string | undefined {
-
-    let accessToken = '';
-    const tokenSegmen = request.headers.authorization?.split(" ");
-    if(tokenSegmen) {
-      accessToken = tokenSegmen[1];
+  /**
+   * Handle request and verify if exist an error or there's not user
+   * @param error
+   * @param user
+   * @returns user || error
+   */
+   override handleRequest(error: Error, user: any) {
+      if (error || !user) {
+        throw new UnauthorizedException();
+      }
+      return user;
     }
-    
-    return accessToken;
-  }
 }
 
 // export default JwtAuthGuard;
